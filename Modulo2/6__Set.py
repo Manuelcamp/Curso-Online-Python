144a15d3605193e15dc38e177bc7a2bf26bf9fad
Manuel Almeida <ywhisgod7@gmail.com>
2026-08-18 16:03:15 -0300

    Methods Set

--- Modulo2/6__Set.py
+++ Modulo2/6__Set.py
@@ -8,8 +8,22 @@
 #Sets não garantem ordem!
 #Set é imutavél, e só aceita valores imutáveis
 #Sem indices!
-s1 = {1, 2, 3}
-#Sets eliminam naturalmente valores duplicados
-for numero in s1:
-    print(numero)
+# s1 = {1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 2, 2, 2, 3, 3}
+# #Sets eliminam naturalmente valores duplicados
+# for numero in s1:
+#     print(numero)
 
+
+
+#Métodos em Set
+#Métodos de adicionar
+s1= set()
+s1.add('Luiz')
+s1.add(1)
+#Update pode mandar vário valores, mas atenção! Caso mande apenas a frase 'Olá mundo!' será separado cada char da frase
+s1.update(('Olá mundo!', 1,2,3,4))
+#Limpa tudo no Set
+# s1.clear()
+#Remove um valor especifico de acordo com oque é passado!
+s1.discard('Luiz')
+print(s1)
